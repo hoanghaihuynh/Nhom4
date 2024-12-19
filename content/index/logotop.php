b2f15093b0673b26e5a289376c937c31b8984c3a
duyban <duybanit123@hotmail.com>
2024-12-19 15:54:41 +0700

    feat(logo):them logo

--- content/index/logotop.php
+++ content/index/logotop.php
@@ -9,3 +9,22 @@ if (isset($_SESSION['cart'])) {
         $count++;
     }
 }
+?>
+<div class="site-branding-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="logo">
+                    <h1><a href=" <?php echo $level."index.php"?>"><img src='<?php echo $level.IMG."NDB.png";?>'></a></h1>
+                </div>
+            </div>
+
+            <div class='col-sm-8'>
+                <div class="shopping-item">
+                    <a href="<?php echo $level.PAGE.'cart.php';?>" style='font-size: 22px'>Giỏ Hàng : <span class="cart-amunt"><?php echo number_format($TongTien, 0, ',', '.')." vnđ"; ?></span><i class="fa fa-shopping-cart"></i><span class="product-count"><?php echo $count; ?></span></a>
+                </div>
+            </div>
+        </div>  
+    </div>
+</div>
+
