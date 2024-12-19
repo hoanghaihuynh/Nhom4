77c0f7a65e35a4e4f1af7b3bdcef8068124576cc
Tempest <hoangcong25022003@gmail.com>
2024-12-19 15:48:01 +0700

    feat(feature/login-register): Giao dien dang nhap

--- login.php
+++ login.php
@@ -40,8 +40,48 @@ $level = Trang0;
 </head>
 
 <body>
+<div class="limiter">
+        <div class="container-login100" style="background-image: url('<?php echo $level . IMG . "bg-01.jpg"?>');">
+            <div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+                <form class="login100-form validate-form flex-sb flex-w" action='' method="POST">
+                    <span class="login100-form-title p-b-53">Sign In With</span>
+                    <div class="p-t-31 p-b-9">
+                        <span class="txt1">Username</span>
+                    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Username is required">
+                        <input class="input100" type="text" name="username" id="username" required>
+                        <span class="focus-input100"></span>
+                    </div>
 
-<!-- div class limiter ở đây -->
+                    <div class="p-t-13 p-b-9" data-toggle="modal">
+                        <span class="txt1">Password</span>
+                        <!-- <a href="forgot.php" class="txt2 bo1 m-l-5">Quên mật khẩu?</a> -->
+                    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="password" id="password" required>
+                        <span class="focus-input100"></span>
+                    </div>
+                    <div>
+                    <br>
+                        
+                    </div>
+
+                    <div class="container-login100-form-btn m-t-17">
+                        <button type="submit" class="login100-form-btn" name="signin">Sign In</button>
+                    </div>
+
+                    <div class="w-full text-center p-t-55">
+                        <span class="txt2">Not a member?</span>
+                        <a href="" class="txt2 bo1">Sign up now</a>
+                    </div>
+                    
+                </form>
+                
+            </div>
+            
+        </div>
+        
+    </div>
 
 </body>
 
